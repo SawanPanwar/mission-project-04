8035cf4725546ae750635294926cd37d36509537
SawanPanwar <skpanwar25@gmail.com>
2024-10-10 12:33:02 +0530

    done

---
+++ Project-04/src/main/java/in/co/rays/test/TestUserModel.java
@@ -0,0 +1,37 @@
+package in.co.rays.test;
+
+public class TestUserModel {
+
+	public static void main(String[] args) throws Exception {
+		testAdd();
+		// testUpdate();
+		// testDelete();
+		// testFindByPk();
+		// testFindByLogin();
+		// testSearch();
+	}
+
+	public static void testAdd() throws Exception {
+
+	}
+
+	public static void testUpdate() throws Exception {
+	}
+
+	public static void testDelete() throws Exception {
+
+	}
+
+	public static void testFindByPk() throws Exception {
+
+	}
+
+	public static void testFindByLogin() throws Exception {
+
+	}
+
+	public static void testSearch() throws Exception {
+
+	}
+
+}
