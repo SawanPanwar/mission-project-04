9fb0db4329b50b016a50f794d588ebac239ac47d
SawanPanwar <skpanwar25@gmail.com>
2024-10-17 18:12:25 +0530

    done

--- Project-04/src/main/java/in/co/rays/ctl/BaseCtl.java
+++ Project-04/src/main/java/in/co/rays/ctl/BaseCtl.java
@@ -1,8 +1,19 @@
 package in.co.rays.ctl;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
-public class BaseCtl extends HttpServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.rays.bean.BaseBean;
+import in.co.rays.bean.UserBean;
+import in.co.rays.util.DataUtility;
+import in.co.rays.util.DataValidator;
+import in.co.rays.util.ServletUtility;
+
+public abstract class BaseCtl extends HttpServlet {
 
 	public static final String OP_SAVE = "Save";
 	public static final String OP_UPDATE = "Update";
@@ -19,14 +30,85 @@ public class BaseCtl extends HttpServlet {
 	public static final String OP_RESET = "Reset";
 	public static final String OP_LOG_OUT = "Logout";
 
-	/**
-	 * Success message key constant
-	 */
 	public static final String MSG_SUCCESS = "success";
 
-	/**
-	 * Error message key constant
-	 */
 	public static final String MSG_ERROR = "error";
+
+	protected boolean validate(HttpServletRequest request) {
+		return true;
+	}
+
+	protected void preload(HttpServletRequest request) {
+	}
+
+	protected BaseBean populateBean(HttpServletRequest request) {
+		return null;
+	}
+
+	protected BaseBean populateDTO(BaseBean dto, HttpServletRequest request) {
+
+		String createdBy = request.getParameter("createdBy");
+		String modifiedBy = null;
+
+		UserBean userbean = (UserBean) request.getSession().getAttribute("user");
+
+		if (userbean == null) {
+			// If record is created without login
+			createdBy = "root";
+			modifiedBy = "root";
+		} else {
+
+			modifiedBy = userbean.getLogin();
+
+			// If record is created first time
+			if ("null".equalsIgnoreCase(createdBy) || DataValidator.isNull(createdBy)) {
+				createdBy = modifiedBy;
+			}
+
+		}
+
+		dto.setCreatedBy(createdBy);
+		dto.setModifiedBy(modifiedBy);
+
+		long cdt = DataUtility.getLong(request.getParameter("createdDatetime"));
+
+		if (cdt > 0) {
+			dto.setCreatedDatetime(DataUtility.getTimestamp(cdt));
+		} else {
+			dto.setCreatedDatetime(DataUtility.getCurrentTimestamp());
+		}
+
+		dto.setModifiedDatetime(DataUtility.getCurrentTimestamp());
+
+		return dto;
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("in base ctl service");
+
+		preload(request);
+
+		String op = DataUtility.getString(request.getParameter("operation"));
+
+		if (DataValidator.isNotNull(op) && !OP_CANCEL.equalsIgnoreCase(op) && !OP_VIEW.equalsIgnoreCase(op)
+				&& !OP_DELETE.equalsIgnoreCase(op) && !OP_RESET.equalsIgnoreCase(op)) {
+
+			if (!validate(request)) {
+				System.out.println("validate return");
+				BaseBean bean = (BaseBean) populateBean(request);
+				ServletUtility.setBean(bean, request);
+				ServletUtility.forward(getView(), request, response);
+				return;
+			}
+		}
+
+		System.out.println("base ctl service" + request.getMethod());
+		super.service(request, response);
+	}
+
+	protected abstract String getView();
 
 }
