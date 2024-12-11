c6a2ec70982d39592e86111588c690d6404a4107
SawanPanwar <skpanwar25@gmail.com>
2024-12-11 17:09:31 +0530

    done

--- ORSProject-04/src/main/java/in/co/rays/test/TestRoleModel.java
+++ ORSProject-04/src/main/java/in/co/rays/test/TestRoleModel.java
@@ -2,6 +2,8 @@ package in.co.rays.test;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import in.co.rays.bean.RoleBean;
 import in.co.rays.model.RoleModel;
@@ -9,17 +11,19 @@ import in.co.rays.model.RoleModel;
 public class TestRoleModel {
 
 	public static void main(String[] args) throws Exception {
-
-		testAdd();
-
+		// testAdd();
+		// testUpdate();
+		// testDelete();
+		// testFindByPk();
+		testFindByName();
+		// testSearch();
 	}
 
-	private static void testAdd() throws Exception {
-		
+	public static void testAdd() throws Exception {
+
 		RoleBean bean = new RoleBean();
-		bean.setId(5);
-		bean.setName("sales");
-		bean.setDescription("sales");
+		bean.setName("kiosk");
+		bean.setDescription("kiosk");
 		bean.setCreatedBy("admin@gmail.com");
 		bean.setModifiedBy("admin@gmail.com");
 		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
@@ -29,5 +33,86 @@ public class TestRoleModel {
 
 		model.add(bean);
 
+	}
+
+	public static void testUpdate() throws Exception {
+
+		RoleBean bean = new RoleBean();
+		bean.setId(2);
+		bean.setName("student");
+		bean.setDescription("student");
+		bean.setCreatedBy("admin@gmail.com");
+		bean.setModifiedBy("admin@gmail.com");
+		bean.setCreatedDatetime(new Timestamp(new Date().getTime()));
+		bean.setModifiedDatetime(new Timestamp(new Date().getTime()));
+
+		RoleModel model = new RoleModel();
+
+		model.update(bean);
+	}
+
+	public static void testDelete() throws Exception {
+		RoleModel model = new RoleModel();
+		model.delete(1);
+	}
+
+	public static void testFindByPk() throws Exception {
+
+		RoleModel model = new RoleModel();
+
+		RoleBean bean = model.findByPk(1);
+
+		if (bean != null) {
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getDescription());
+			System.out.print("\t" + bean.getCreatedBy());
+			System.out.print("\t" + bean.getModifiedBy());
+			System.out.print("\t" + bean.getCreatedDatetime());
+			System.out.println("\t" + bean.getModifiedDatetime());
+		} else {
+			System.out.println("id not found");
+		}
+	}
+
+	public static void testFindByName() throws Exception {
+
+		RoleModel model = new RoleModel();
+
+		RoleBean bean = model.findByName("admin");
+
+		if (bean != null) {
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getDescription());
+			System.out.print("\t" + bean.getCreatedBy());
+			System.out.print("\t" + bean.getModifiedBy());
+			System.out.print("\t" + bean.getCreatedDatetime());
+			System.out.println("\t" + bean.getModifiedDatetime());
+		} else {
+			System.out.println("name not found");
+		}
+	}
+
+	public static void testSearch() throws Exception {
+
+		RoleBean bean = new RoleBean();
+
+		RoleModel model = new RoleModel();
+
+		List list = model.search(bean, 1, 10);
+
+		Iterator it = list.iterator();
+
+		while (it.hasNext()) {
+			bean = (RoleBean) it.next();
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getName());
+			System.out.print("\t" + bean.getDescription());
+			System.out.print("\t" + bean.getCreatedBy());
+			System.out.print("\t" + bean.getModifiedBy());
+			System.out.print("\t" + bean.getCreatedDatetime());
+			System.out.println("\t" + bean.getModifiedDatetime());
+		}
 	}
 }
