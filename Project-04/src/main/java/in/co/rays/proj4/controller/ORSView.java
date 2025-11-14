cbecb16e5323d4c108e6df1e529039e78d236b18
SawanPanwar <skpanwar25@gmail.com>
2025-11-14 17:24:26 +0530

    14-Nov-2025

---
+++ Project-04/src/main/java/in/co/rays/proj4/controller/ORSView.java
@@ -0,0 +1,15 @@
+package in.co.rays.proj4.controller;
+
+public interface ORSView {
+
+	public String APP_CONTEXT = "/Project-04";
+
+	public String PAGE_FOLDER = "/jsp";
+
+	public String WELCOME_VIEW = PAGE_FOLDER + "/Welcome.jsp";
+	public String WELCOME_CTL = APP_CONTEXT + "/WelcomeCtl";
+
+	public String USER_REGISTRATION_VIEW = PAGE_FOLDER + "/UserRegistrationView.jsp";
+	public String USER_REGISTRATION_CTL = APP_CONTEXT + "/UserRegistrationCtl";
+
+}
