43fc2b1b391451f63a2838bac4c06f08954e79da
SawanPanwar <skpanwar25@gmail.com>
2025-11-17 17:20:08 +0530

    17 November 2025

--- Project-04/src/main/java/in/co/rays/proj4/controller/LoginCtl.java
+++ Project-04/src/main/java/in/co/rays/proj4/controller/LoginCtl.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import in.co.rays.proj4.bean.BaseBean;
 import in.co.rays.proj4.bean.UserBean;
+import in.co.rays.proj4.util.DataUtility;
+import in.co.rays.proj4.util.DataValidator;
+import in.co.rays.proj4.util.PropertyReader;
 import in.co.rays.proj4.util.ServletUtility;
 
 @WebServlet(name = "LoginCtl", urlPatterns = { "/LoginCtl" })
@@ -19,13 +22,34 @@ public class LoginCtl extends BaseCtl {
 
 	@Override
 	protected boolean validate(HttpServletRequest request) {
+
 		boolean pass = true;
+
+		String op = request.getParameter("operation");
+
+		if (OP_SIGN_UP.equals(op) || OP_LOG_OUT.equals(op)) {
+			return pass;
+		}
+
+		if (DataValidator.isNull(request.getParameter("login"))) {
+			request.setAttribute("login", PropertyReader.getValue("error.require", "Login Id"));
+			pass = false;
+		} else if (!DataValidator.isEmail(request.getParameter("login"))) {
+			request.setAttribute("login", PropertyReader.getValue("error.email", "Login "));
+			pass = false;
+		}
+		if (DataValidator.isNull(request.getParameter("password"))) {
+			request.setAttribute("password", PropertyReader.getValue("error.require", "Password"));
+			pass = false;
+		}
 		return pass;
 	}
 
 	@Override
 	protected BaseBean populateBean(HttpServletRequest request) {
 		UserBean bean = new UserBean();
+		bean.setLogin(DataUtility.getString(request.getParameter("login")));
+		bean.setPassword(DataUtility.getString(request.getParameter("password")));
 		return bean;
 	}
 
@@ -36,6 +60,15 @@ public class LoginCtl extends BaseCtl {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		String op = DataUtility.getString(request.getParameter("operation"));
+
+		if (OP_SIGN_IN.equalsIgnoreCase(op)) {
+
+		} else if (OP_SIGN_UP.equalsIgnoreCase(op)) {
+			ServletUtility.redirect(ORSView.USER_REGISTRATION_CTL, request, response);
+			return;
+		}
 		ServletUtility.forward(getView(), request, response);
 	}
 
